be529ede01c686735135819a6402b233f4bedd62
bongbong4043 <bong4043@paran.com>
2023-11-30 11:16:09 +0900

    maven-junt project, initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n.....................");
+        System.out.println("Hello Calculator junit5 test... main");
+        System.out.println(".....................\n");
+
+    }
+}
